08296fd64f1129698694bed718b4c3b1b1d33aac
redemption-001 <83798725+redemption-001@users.noreply.github.com>
2022-10-07 21:07:14 -0300

    feat: :recycle: Replace throwing exceptions with notification in products' validations (#4)

--- src/domain/@shared/notification/notification.ts
+++ src/domain/@shared/notification/notification.ts
@@ -1,3 +1,5 @@
+import NotificationError from "./notification.error";
+
 export type NotificationErrorProps = {
   message: string;
   context: string;
@@ -26,5 +28,12 @@ export default class Notification {
       }
     });
     return message;
+  }
+
+  notificate(context?: string){   
+    let errorsList = this.errors.filter(err => context === undefined || err.context === context);
+    if (errorsList.length > 0) {
+      throw new NotificationError(errorsList);
+    }
   }
 }
--- src/domain/product/entity/product.spec.ts
+++ src/domain/product/entity/product.spec.ts
@@ -1,22 +1,35 @@
 import Product from "./product";
+import ProductB from "./product-b";
 
 describe("Product unit tests", () => {
   it("should throw error when id is empty", () => {
     expect(() => {
       const product = new Product("", "Product 1", 100);
-    }).toThrowError("Id is required");
+    }).toThrowError("product: Id is required");
   });
 
   it("should throw error when name is empty", () => {
     expect(() => {
       const product = new Product("123", "", 100);
-    }).toThrowError("Name is required");
+    }).toThrowError("product: Name is required");
   });
 
   it("should throw error when price is less than zero", () => {
     expect(() => {
       const product = new Product("123", "Name", -1);
-    }).toThrowError("Price must be greater than zero");
+    }).toThrowError("product: Price must be greater than zero");
+  });
+
+  it("should throw error when: id and name are empty and price is less than zero", () => {
+    expect(() => {
+      const product = new Product("", "", -1);
+    }).toThrowError("product: Id is required,product: Name is required,product: Price must be greater than zero");
+  });
+
+  it("should throw error when: id and name are empty and price is less than zero", () => {
+    expect(() => {
+      const product = new ProductB("", "", -1);
+    }).toThrowError("product: Id is required,product: Name is required,product: Price must be greater than zero");
   });
 
   it("should change name", () => {
